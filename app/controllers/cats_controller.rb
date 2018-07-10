94ac5e161b0b0d2be8cc8565c9c6f9ff32b5fa7a
z-nr <zayneyre@gmail.com>
2018-07-10 15:37:43 -0700

    complete cats controller

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -9,6 +9,34 @@ class CatsController < ApplicationController
     render :show
   end
   
+  def new
+    @cat = Cat.new
+    render :new
+  end
+  
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+  
+  def update 
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      redirect_to edit_cat_url(@cat)
+    end 
+  end 
+  
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save 
+      redirect_to cat_url(@cat)
+    else
+      redirect_to new_cat_url
+    end 
+  end 
+  
   private 
   def cat_params
     params.require(:cat).permit(:birth_date,:color,:name,:sex,:description)
